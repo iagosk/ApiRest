560e58950590ad1e88ca30f16cda54e67cfcfcab
Matheus Iago dos Santos Silva <162377902+iagosk@users.noreply.github.com>
2026-02-04 23:03:06 -0300

    Update backend
    - Estabelecendo a conexão da api com o servidor do banco de dados postgresql.
    - Rondando a primeira migração do projeto.
    - Criando o usuário admin do django.

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbApiRest',
+        'PASSWORD':'iagohtml5',
+        'USER':'postgres',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 

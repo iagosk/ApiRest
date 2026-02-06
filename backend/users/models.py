f5150195ccec28bef16f550de2817dd30cfe2f50
Matheus Iago dos Santos Silva <162377902+iagosk@users.noreply.github.com>
2026-02-05 23:01:47 -0300

    Update backend
    - Correção no modelo de dados do user.
    - Registro do modelo na seção de admin do django.

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class User(models.Model):
     fullName = models.CharField(max_length=100)
     age = models.IntegerField()
-    nameUser = models.CharField(100)
-    password = models.CharField(100)
+    nameUser = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
 
 def __str__(self):
     return self.name

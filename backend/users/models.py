50ebc286deb0a39447177657cec923948ffb2d3b
Matheus Iago dos Santos Silva <162377902+iagosk@users.noreply.github.com>
2026-02-05 22:51:30 -0300

    Update backend
    - Correção nas referências do cors.
    - Definição da viewset dos users.
    - Definição da classe serializer dos users.
    - Definição da rota 'users/' para acessar os users cadastrados no sistema.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    fullName = models.CharField(max_length=100)
+    age = models.IntegerField()
+    nameUser = models.CharField(100)
+    password = models.CharField(100)
+
+def __str__(self):
+    return self.name
